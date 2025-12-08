a33fa4998f0ac196bda3cc8195fe77b21c9f8e1b
NickoIkhwan <162401382+NickoIkhwan@users.noreply.github.com>
2025-12-08 18:24:28 +0700

    boost jittering fixed
    todo:
    -add shooting mechanics
    -add AI npcs

--- src/Player/boost.gd
+++ src/Player/boost.gd
@@ -3,6 +3,8 @@ extends Node
 signal boost_status(boost : float)
 @onready var Player = $"../.."
 var boost : float = 100.0
+var consume_rate : float = 100.0
+var recharge_rate: float = 120.0
 var is_boosting : bool = false
 var dashing : bool = false
 
@@ -13,14 +15,16 @@ func _on_character_body_3d_consume_boost(is_consuming_boost: bool) -> void:
 	is_boosting = is_consuming_boost
 	
 func _process(delta: float) -> void:
-
 		
 	if is_boosting:
-		boost -= 100.0 * delta
-	elif boost < 100.0 and Player.is_on_floor():
-		boost += 800.0 * delta
-	
-	
+		boost -= consume_rate * delta
+	elif boost < 100.0  and Player.is_on_floor():
+		boost += recharge_rate * delta
+
+	if boost > 100:
+		boost = 100
+		
 	if boost_status:
 		boost_status.emit(boost)
+
 	
--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,11 +1,11 @@
-class_name Player extends CharacterBody3D
+extends CharacterBody3D
 
 signal consume_boost(is_consuming_boost:bool)
 signal dash(is_dashing : bool)
 
-@export var speed : float = 10.0
-@export var fly_speed : float = 10.0
-@export var gravity : float = 30.0
+const SPEED : float = 10.0
+const FLY_SPEED : float = 25.0
+const DASH_MULTIPLIER : float = 4
 @onready var boost_Gauge : ProgressBar = %ProgressBar
 var accel : float = 5.0
 var deaccel : float = 6.0
@@ -14,6 +14,7 @@ var deaccel_dash : float = 0.1
 var boost_player : float 
 var is_dashing : bool = false
 var is_consuming_boost : bool = false
+var dash_locked : bool = false
 
 
 func _on_boost_boost_status(boost: float) -> void:
@@ -21,38 +22,59 @@ func _on_boost_boost_status(boost: float) -> void:
 	
 func _physics_process(delta: float) -> void:
 	
-
+	var input_dir = Input.get_vector("left","right","forward","back")
+	var input_dir3D = Vector3( input_dir.x, 0.0, input_dir.y)
+	var direction = %CamYaw.transform.basis * input_dir3D
+	var target_acc = direction * SPEED
+	var current = accel
+	
+	if input_dir3D.length_squared() > 0:
+		var target_basis = basis.looking_at( -direction, Vector3.UP)
+		%Leg.global_transform.basis = %Leg.global_transform.basis.slerp(target_basis, 10.2 * delta)
+	
+	if not is_dashing:
+		velocity.y -= 30.0 * delta
+	
 	if boost_player >= 0:
 		if Input.is_action_pressed("jump"):
-			velocity.y = lerp(velocity.y, fly_speed, 10.0 * delta)
+			velocity.y = lerp(velocity.y, FLY_SPEED, 10.0 * delta)
 			is_consuming_boost = true
 			consume_boost.emit(is_consuming_boost)
-		elif Input.is_action_pressed("dash"):
+		elif Input.is_action_pressed("dash") and not dash_locked:
 			if Input.is_action_pressed("movement"):
+				target_acc *= DASH_MULTIPLIER
 				velocity.y = 0.0
-				is_consuming_boost = true
-				is_dashing = true
-				consume_boost.emit(is_consuming_boost)
-				dash.emit(is_dashing)
+				dashing_signal_true()
 		else:
-			is_consuming_boost = false
-			is_dashing = false
-			consume_boost.emit(is_consuming_boost)
-			dash.emit(is_dashing)
+			dashing_signal_false()
 	else:
-		is_consuming_boost = false
-		is_dashing = false
-		consume_boost.emit(is_consuming_boost)
-		dash.emit(is_dashing)
-		
-	#if target_acc.length_squared()==0:
-		#if is_dashing:
-	#		current = deaccel_dash
-	#	elif not is_on_floor():
-	#		current = deaccel_air
-	#	else:
-	#		current = deaccel
-		
+		dashing_signal_false()
+		dash_locked = true
 	
+	if not Input.is_action_pressed("dash"):
+		dash_locked = false
 		
+	if target_acc.length_squared()==0:
+		if is_dashing:
+			current = deaccel_dash
+		elif not is_on_floor():
+			current = deaccel_air
+		else:
+			current = deaccel
+		
+	velocity.x = lerp(velocity.x, target_acc.x, current * delta)
+	velocity.z = lerp(velocity.z, target_acc.z, current * delta)
+	
 	move_and_slide()
+	
+func dashing_signal_true():
+	is_consuming_boost = true
+	is_dashing = true
+	consume_boost.emit(is_consuming_boost)
+	dash.emit(is_dashing)
+	
+func dashing_signal_false():
+	is_consuming_boost = false
+	is_dashing = false
+	consume_boost.emit(is_consuming_boost)
+	dash.emit(is_dashing)
